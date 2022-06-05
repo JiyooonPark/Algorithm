7949d429465fed3e94e2cc8a534cfc90246e09bd
jypark <jiyoonpark0207@gmail.com>
2022-06-05 16:49:50 +0900

    220605 bfs

--- ThisIsTheRealCodingTest/220605/bfs_basic.py
+++ ThisIsTheRealCodingTest/220605/bfs_basic.py
@@ -1,12 +1,47 @@
-# from collections import deque
-#
-# queue = deque()
-#
+from collections import deque
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
 # queue.append(1)
-# queue.append(2)
-# queue.append(3)
-# queue.append(4)
-# queue.append(5)
-# print(queue)
 # queue.popleft()
-# print(queue)
+
+# mine bfs
+visited = [False]*9
+def bfs1(n, graph, visited):
+
+    queue = deque()
+    queue.append(n)
+
+    while not len(queue) == 0:
+        n = queue.popleft()
+        print(n,end=' ')
+
+        visited[n]=True
+        for i in graph[n]:
+            if visited[i] == False and i not in queue:
+                queue.append(i)
+
+# bfs1(1, graph, visited)
+
+# book answer
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+bfs(graph, 1, visited)
+
